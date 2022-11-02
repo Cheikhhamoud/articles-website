df998df2c8db6605a6078866a888246795a2d24b
Cheikh <cheikh56@gmail.com>
2022-11-02 02:21:36 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,57 @@
+//  To controll ur website
+
+const express = require("express");
+const app = express();
+const dotenv = require("dotenv");
+dotenv.config()
+
+
+
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+
+// To import routes file
+const allArticlesRouter = require("./routes/all-articles");
+
+// for auto refresh
+const path = require("path");
+const livereload = require("livereload");
+const liveReloadServer = livereload.createServer();
+liveReloadServer.watch(path.join(__dirname, "public"));
+
+const connectLivereload = require("connect-livereload");
+app.use(connectLivereload());
+
+liveReloadServer.server.once("connection", () => {
+  setTimeout(() => {
+    liveReloadServer.refresh("/");
+  }, 100);
+});
+
+// mongoose
+const mongoose = require("mongoose");
+
+mongoose.connect(process.env.MONGO_URI, () => console.log('db conected'));
+
+
+const port = process.env.PORT
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
+
+app.get("/", (req, res) => {
+  res.redirect("/all-articles");
+});
+
+app.get("/add-new-article", (req, res) => {
+  res.render("add-new-article", { mytitle: "create new article" });
+});
+
+// all-articles PATH
+app.use("/all-articles", allArticlesRouter);
+
+//  404
+app.use((req, res) => {
+  res.status(404).send("Sorry can't find that!");
+});
